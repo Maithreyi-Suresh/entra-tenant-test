549af92ef109288421315c889a29fb7a84d241dd
Maithreyi-Suresh <125529365+Maithreyi-Suresh@users.noreply.github.com>
2025-12-05 13:49:35 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,13 @@
-// config.js
-// Filled by the Entra/B2C team
-
 window.msalSettings = {
-  clientId: "13a102ca-c915-4897-8416-67ab0b262539", // example
-  authority:
-    "https://leapgenAIExternal.ciamlogin.com", 
-  // For B2C/External, you usually use a policy-based URL like:
-  // "https://<tenant>.ciamlogin.com/<tenant>.onmicrosoft.com/<flow_name>"
-
-  redirectUri: "https://maithreyi-suresh.github.io/entra-tenant-test",
-
-  // Optional, recommended for B2C:
-  //knownAuthorities: ["leapgenAIExternal.ciamlogin.com"],
+  auth: {
+    clientId: "13a102ca-c915-4897-8416-67ab0b262539",
+    authority:
+      "https://leapgenAIExternal.ciamlogin.com/leapgenAIExternal.onmicrosoft.com/B2C_1_signup_signin", // replace with your actual flow name
+    redirectUri: "https://maithreyi-suresh.github.io/entra-tenant-test",
+    knownAuthorities: ["leapgenAIExternal.ciamlogin.com"],
+  },
+  cache: {
+    cacheLocation: "sessionStorage",
+    storeAuthStateInCookie: false,
+  },
 };
-
-
-
-
-
