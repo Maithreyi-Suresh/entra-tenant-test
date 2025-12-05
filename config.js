525527c86cf0f0c488e1c6a78454bf3ccb5f3b35
Maithreyi-Suresh <125529365+Maithreyi-Suresh@users.noreply.github.com>
2025-12-05 08:16:33 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+// config.js
+// Filled by the Entra/B2C team
+
+window.msalSettings = {
+  clientId: "13a102ca-c915-4897-8416-67ab0b262539", // example
+  authority:
+    "https://leapgenAIExternal.ciamlogin.com/leapgenAIExternal.onmicrosoft.com/oauth2/v2.0/authorize", 
+  // For B2C/External, you usually use a policy-based URL like:
+  // "https://<tenant>.ciamlogin.com/<tenant>.onmicrosoft.com/<flow_name>"
+
+  redirectUri: "https://maithreyi-suresh.github.io/entra-tenant-test",
+
+  // Optional, recommended for B2C:
+  knownAuthorities: ["leapgenAIExternal.ciamlogin.com"],
+};
