5aee585e0df082409e16782f5fadcf74c1f42ae5
Maithreyi-Suresh <125529365+Maithreyi-Suresh@users.noreply.github.com>
2025-12-05 08:42:50 -0500

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 window.msalSettings = {
   clientId: "13a102ca-c915-4897-8416-67ab0b262539", // example
   authority:
-    "https://leapgenAIExternal.ciamlogin.com/leapgenAIExternal.onmicrosoft.com/B2C_1_signup_signin", 
+    "https://leapgenAIExternal.ciamlogin.com/leapgenAIExternal.onmicrosoft.com/selfasserted", 
   // For B2C/External, you usually use a policy-based URL like:
   // "https://<tenant>.ciamlogin.com/<tenant>.onmicrosoft.com/<flow_name>"
 
@@ -13,4 +13,5 @@ window.msalSettings = {
   // Optional, recommended for B2C:
   knownAuthorities: ["leapgenAIExternal.ciamlogin.com"],
 };
+
 
