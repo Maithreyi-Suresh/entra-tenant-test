5f73014eff95564ff36c4428679ca9c1d281a37b
Maithreyi-Suresh <125529365+Maithreyi-Suresh@users.noreply.github.com>
2025-12-05 13:55:45 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.msalSettings = {
   auth: {
     clientId: "13a102ca-c915-4897-8416-67ab0b262539",
     authority:
-      "https://leapgenAIExternal.ciamlogin.com/leapgenAIExternal.onmicrosoft.com/B2C_1_signup_signin", // replace with your actual flow name
+      "https://leapgenAIExternal.ciamlogin.com/leapgenAIExternal.onmicrosoft.com/SignupandSignIn", // replace with your actual flow name
     redirectUri: "https://maithreyi-suresh.github.io/entra-tenant-test",
     knownAuthorities: ["leapgenAIExternal.ciamlogin.com"],
   },
@@ -11,3 +11,15 @@ window.msalSettings = {
     storeAuthStateInCookie: false,
   },
 };
+
+const msalConfig = {
+  auth: {
+    clientId: window.msalSettings.clientId,
+    authority: window.msalSettings.authority,
+    redirectUri: window.msalSettings.redirectUri,
+    knownAuthorities: window.msalSettings.knownAuthorities,
+  },
+};
+
+const msalInstance = new msal.PublicClientApplication(msalConfig);
+
