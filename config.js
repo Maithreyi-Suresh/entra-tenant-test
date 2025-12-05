21315f0b4bb3d1db2062c0670d8dfe1f70e11fb1
Maithreyi-Suresh <125529365+Maithreyi-Suresh@users.noreply.github.com>
2025-12-05 13:27:54 -0500

    Update config.js

--- config.js
+++ config.js
@@ -4,14 +4,15 @@
 window.msalSettings = {
   clientId: "13a102ca-c915-4897-8416-67ab0b262539", // example
   authority:
-    "https://leapgenAIExternal.ciamlogin.com/leapgenAIExternal.onmicrosoft.com/selfasserted", 
+    "https://leapgenAIExternal.ciamlogin.com/leapgenAIExternal.onmicrosoft.com", 
   // For B2C/External, you usually use a policy-based URL like:
   // "https://<tenant>.ciamlogin.com/<tenant>.onmicrosoft.com/<flow_name>"
 
   redirectUri: "https://maithreyi-suresh.github.io/entra-tenant-test",
 
   // Optional, recommended for B2C:
-  knownAuthorities: ["leapgenAIExternal.ciamlogin.com"],
+  //knownAuthorities: ["leapgenAIExternal.ciamlogin.com"],
 };
+
 
 
